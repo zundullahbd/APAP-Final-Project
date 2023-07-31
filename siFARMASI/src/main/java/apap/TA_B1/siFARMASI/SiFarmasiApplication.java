0b8b1f3304040e96b28f00228f4e156a92225c47
zundullahbd <uluzundullah@gmail.com>
2023-07-31 23:27:46 +0700

    feat;doc;chore: initial commit

---
+++ siFARMASI/src/main/java/apap/TA_B1/siFARMASI/SiFarmasiApplication.java
@@ -0,0 +1,13 @@
+package apap.TA_B1.siFARMASI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiFarmasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiFarmasiApplication.class, args);
+	}
+
+}
---
+++ siFARMASI/src/test/java/apap/TA_B1/siFARMASI/SiFarmasiApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.TA_B1.siFARMASI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiFarmasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
