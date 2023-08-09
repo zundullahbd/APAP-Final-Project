54a6cd40b44b6350566f99e9c271f210be5bd325
MassiveMassimo <muhammad.hannan@ui.ac.id>
2023-08-10 00:21:55 +0700

    feat: finished CRUD of mitra

---
+++ siFARMASI/src/main/java/apap/TA_B1/siFARMASI/controller/MitraController.java
@@ -0,0 +1,67 @@
+package apap.TA_B1.siFARMASI.controller;
+
+import apap.TA_B1.siFARMASI.model.MitraModel;
+import apap.TA_B1.siFARMASI.service.MitraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/mitra")
+public class MitraController {
+    @Autowired
+    private MitraService mitraService;
+
+    @GetMapping("")
+    public String viewMitraList(Model model) {
+        List<MitraModel> listMitra = mitraService.getListMitra();
+        model.addAttribute("listMitra", listMitra);
+        return "mitra/list-mitra";
+    }
+
+    @GetMapping("/add")
+    public String addMitraForm(Model model) {
+        model.addAttribute("mitraModel", new MitraModel());
+        return "mitra/form-add-mitra";
+    }
+
+    @PostMapping("/add")
+    public String addMitraSubmit(@ModelAttribute MitraModel mitra) {
+        mitraService.addMitra(mitra);
+        return "redirect:/mitra";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMitraForm(@PathVariable Integer id, Model model) {
+        MitraModel mitra = mitraService.getMitraById(id);
+        model.addAttribute("mitra", mitra);
+        return "mitra/form-update-mitra";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editMitraSubmit(@PathVariable Integer id, @ModelAttribute @Valid MitraModel mitra,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "mitra/form-update-mitra";
+        }
+        mitra.setId(id);
+        mitraService.updateMitra(mitra);
+        return "redirect:/mitra";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMitra(@PathVariable Integer id) {
+        mitraService.deleteMitra(id);
+        return "redirect:/mitra";
+    }
+}
--- siFARMASI/src/main/java/apap/TA_B1/siFARMASI/service/MitraService.java
+++ siFARMASI/src/main/java/apap/TA_B1/siFARMASI/service/MitraService.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface MitraService {
     List<MitraModel> getListMitra();
+    MitraModel getMitraById(Integer id);
+    MitraModel addMitra(MitraModel mitra);
+    MitraModel updateMitra(MitraModel mitra);
+    void deleteMitra(Integer id);
 }
