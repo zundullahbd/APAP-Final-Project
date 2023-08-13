6ebf40018df0c415bca0babfb02d51215447fcec
Fikriaffan Fadlil <fikriaffan.fadlil@gmail.com>
2023-08-13 18:34:26 +0700

    chore: resolve naming conflict

--- siFARMASI/src/main/java/apap/TA_B1/siFARMASI/model/MitraModel.java
+++ siFARMASI/src/main/java/apap/TA_B1/siFARMASI/model/MitraModel.java
@@ -27,7 +27,7 @@ public class MitraModel implements Serializable {
 
     @NotBlank
     @Column(name = "name", nullable = false)
-    private String name;
+    private String nama;
 
     @NotBlank
     @Column(name = "alamat", nullable = false)
